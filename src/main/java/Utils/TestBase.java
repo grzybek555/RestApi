3f8a5a065b3ea711119ed9df9ee83e8a5b8b5c4b
Dominik Grzybowski <dominik.grzybowski1@gmail.com>
2020-05-26 00:53:20 +0200

    Added jsons schema, categories by id test, parameters test

--- src/main/java/Utils/TestBase.java
+++ src/main/java/Utils/TestBase.java
@@ -2,8 +2,10 @@ package Utils;
 
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.annotations.BeforeClass;
 
@@ -11,8 +13,15 @@ import static io.restassured.RestAssured.*;
 
 public class TestBase {
     private String API_TOKEN;
+    public static final String CATEGORIES_API_PATH = "/sale/categories/";
     public static final String CONTENT_TYPE = "application/vnd.allegro.public.v1+json";
-    Logger logger = Logger.getLogger(TestBase.class);
+
+    public static final int RESPONSE_OK_CODE = 200;
+    public static final int RESPONSE_UNATHORIZED_CODE = 401;
+    public static final int RESPONSE_ELEMENT_NOT_FOUND_CODE = 404;
+    public static final int RESPONSE_METHOD_NOT_ALLOWED_CODE = 405;
+
+     Logger logger = Logger.getLogger(TestBase.class);
 
 
     @BeforeClass
@@ -37,6 +46,28 @@ public class TestBase {
 
     public String getApiToken(){
         return API_TOKEN;
+    }
+
+    public RequestSpecification getAuthorizedRequest(){
+        return given()
+                .auth()
+                .oauth2(getApiToken())
+                .accept(CONTENT_TYPE);
+    }
+
+    public RequestSpecification getUnauthorizedRequest(){
+        return given()
+                .accept(CONTENT_TYPE);
+    }
+
+    public JSONArray getObjectArrayFromRequest(String requestPath, String objectKey){
+        JSONObject responseAsJson = new JSONObject(getAuthorizedRequest()
+                .when()
+                .get(requestPath)
+                .getBody()
+                .asString());
+
+        return responseAsJson.getJSONArray(objectKey);
     }
 
     private void setApiToken(String apiToken){
--- src/test/java/CategoriesTests.java
+++ src/test/java/CategoriesTests.java
@@ -1,31 +1,124 @@
 import Utils.TestBase;
 import io.restassured.response.Response;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class CategoriesTests extends TestBase {
+
+    Logger logger = Logger.getLogger(CategoriesTests.class);
+
     @Test
-    public void checkCorrectRequestStatusCode(){
-        int expectedCode = 200;
+    public void checkCorrectCategoriesRequestStatusCode(){
 
-         given()
-                 .auth()
-                 .oauth2(getApiToken())
-                 .accept(CONTENT_TYPE)
-         .when()
-                 .get("/sale/categories")
-         .then().assertThat().statusCode(expectedCode);
-
+        getAuthorizedRequest()
+                .when()
+                     .get(CATEGORIES_API_PATH)
+                .then().assertThat().statusCode(RESPONSE_OK_CODE);
     }
 
     @Test
-    public void checkResponseWithoutAuthentication(){
-        Response response = given()
-                .accept(CONTENT_TYPE)
+    public void checkCategoriesResponseWithoutAuthentication(){
+        getUnauthorizedRequest()
                 .when()
-                .get("/sale/categories");
+                    .get(CATEGORIES_API_PATH)
+                .then()
+                    .assertThat()
+                    .statusCode(RESPONSE_UNATHORIZED_CODE);
+    }
 
-        response.then().assertThat().statusCode(401);
+    @Test
+    public void checkCategoriesResponseSchema(){
+        getAuthorizedRequest()
+                .when()
+                    .get(CATEGORIES_API_PATH)
+                .then()
+                    .assertThat()
+                    .body(matchesJsonSchemaInClasspath("categories-schema.json"));
+    }
+
+    @Test
+    public void checkNotAllowedMethodOnCategoriesPath(){
+        getAuthorizedRequest()
+                .when()
+                    .post(CATEGORIES_API_PATH)
+                .then()
+                    .statusCode(RESPONSE_METHOD_NOT_ALLOWED_CODE);
+    }
+
+    @Test
+    public void checkGettingCategoryByCorrectId(){
+        JSONObject testBaseObject = getObjectArrayFromRequest(CATEGORIES_API_PATH, "categories").getJSONObject(0);
+        Response response = getAuthorizedRequest()
+                .when()
+                .get(CATEGORIES_API_PATH+testBaseObject.getString("id"));
+
+        response.then().statusCode(RESPONSE_OK_CODE);
+
+        JSONObject objectGetById = new JSONObject(response.getBody().asString());
+        JSONAssert.assertEquals(testBaseObject, objectGetById, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    public void checkNotAllowedMethodOnCategoryById(){
+        JSONObject testBaseObject = getObjectArrayFromRequest(CATEGORIES_API_PATH, "categories").getJSONObject(0);
+        getAuthorizedRequest()
+                .when()
+                    .post(CATEGORIES_API_PATH+testBaseObject.getString("id"))
+                .then()
+                    .statusCode(RESPONSE_METHOD_NOT_ALLOWED_CODE);
+    }
+
+    @Test
+    public void checkGettingCategoryByCorrectIdSchema(){
+        JSONObject testBaseObject = getObjectArrayFromRequest(CATEGORIES_API_PATH, "categories").getJSONObject(0);
+        getAuthorizedRequest()
+                .when()
+                    .get(CATEGORIES_API_PATH+testBaseObject.getString("id"))
+                .then()
+                    .assertThat()
+                    .body(matchesJsonSchemaInClasspath("category-schema.json"));
+    }
+
+    @Test
+    public void checkGettingCategoryByIncorrectId(){
+        getAuthorizedRequest()
+                .when()
+                    .get(CATEGORIES_API_PATH+"incorrectId")
+                .then()
+                    .statusCode(RESPONSE_ELEMENT_NOT_FOUND_CODE);
+    }
+
+    @Test
+    public void checkCorrectCategoryParameterStatusCode(){
+        JSONObject testBaseObject = getObjectArrayFromRequest(CATEGORIES_API_PATH, "categories").getJSONObject(0);
+        getAuthorizedRequest()
+                .when()
+                    .get(CATEGORIES_API_PATH+testBaseObject.getString("id")+"/parameters")
+                .then()
+                    .statusCode(RESPONSE_OK_CODE);
+    }
+
+    @Test
+    public void checkIncorrectCategoryParameterStatusCode(){
+        getAuthorizedRequest()
+                .when()
+                .get(CATEGORIES_API_PATH+"testIncorrect/parameters")
+                .then()
+                .statusCode(RESPONSE_ELEMENT_NOT_FOUND_CODE);
+    }
+
+    @Test
+    public void checkNotAllowedMethodOnCategoryParameter(){
+        JSONObject testBaseObject = getObjectArrayFromRequest(CATEGORIES_API_PATH, "categories").getJSONObject(0);
+        getAuthorizedRequest()
+                .when()
+                .post(CATEGORIES_API_PATH+testBaseObject.getString("id")+"/parameters")
+                .then()
+                .statusCode(RESPONSE_METHOD_NOT_ALLOWED_CODE);
     }
 }
